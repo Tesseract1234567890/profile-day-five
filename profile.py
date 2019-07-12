f59d62cf6af66c6822778724b9c3d840f5e73aa5
EC2 Default User <ec2-user@ip-172-31-42-122.us-east-2.compute.internal>
2019-07-12 15:46:55 +0000

    first commit

---
+++ profile.py
@@ -0,0 +1,6 @@
+class Profile:
+    def __init__(self, name, age, money, has_dog):
+        self.name = name
+        self.age = age
+        self.money = money
+        self.has_dog = has_dog
---
+++ test.py
@@ -0,0 +1,7 @@
+import profile
+
+profile1 = profile.Profile("Cassidy", 23, 398.32, True)
+profile2 = profile.Profile("Mark", 58, 832987.82, True)
+profile3 = profile.Profile("Shrek", 1203, 9999999.99, True)
+
+print(profile3.name)
